db623b1d1155b3ded9ffc31acc3e8b36a08edb35
Zed_S <zedsnake89@gmail.com>
2025-02-28 15:36:27 +0300

    Добавили логгирование

--- src/main/java/com/example/firstjava/controller/HelloController.java
+++ src/main/java/com/example/firstjava/controller/HelloController.java
@@ -3,6 +3,8 @@ package com.example.firstjava.controller;
 import com.example.firstjava.dto.UserMessageDto;
 import com.example.firstjava.dto.UserMessageRequestDto;
 import com.example.firstjava.service.HelloService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     private final HelloService helloService;
 
@@ -19,6 +23,7 @@ public class HelloController {
 
     @GetMapping()
     public String sayHello() {
+        log.info("Вызван метод sayHello()");
         return helloService.sayHello();
     }
 
